855cb65c5268a11b8e361bda62bd4af7de9158c4
Constance Devanne <devconstance@gmail.com>
2018-04-09 21:57:12 +0200

    New

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,33 @@
-/*Choix du joueur, choix du monde*/
+/*Choix du joueur*/
 let choice = document.querySelectorAll(".choice")
 let player = 0
 
-for(let i = 0; i<choice.length; i++) {
+for(let i = 0; i < choice.length; i++) {
   choice[i].addEventListener('click', function(){
     if (i == 0) {
-      player = "Vous avez choisi un homme !"
+      let ask = prompt("Entrez votre pseudo !")
+      player = (`Bienvenue ${ask} !`)
     }
     else {
-      player = "Vous avez choisi une femme !"
+      ask = prompt("Entrez votre pseudo !")
+      player = (`Bienvenue ${ask} !`)
     }
     document.querySelector("#player").innerHTML = player
+  })
+}
+/*explications et choix sur du monde*/
+let world = document.querySelectorAll(".items")
+let isActive = 0
+
+for(let i = 0; i < world.length; i++) {
+  world[i].addEventListener( 'click', function() {
+    if(isActive == 0) {
+        world[i].style.border=("2px solid red");
+        world[i].style.borderRadius = ("50%");
+        isActive=1;
+    } else{
+        world[i].style.border = ("none");
+        isActive = 0;
+    }
   })
 }
