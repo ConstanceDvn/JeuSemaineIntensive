be66465af6895efd79b465e90e54cadd3a8a2907
Gander Matthias <spam68130@gmail.com>
2018-04-10 14:51:00 +0200

    add map and movement sprite on charac

--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,12 @@
-let perso, persoX, persoY, dir, door
+let perso, persoX, persoY, dir, door, n = 0
 door = document.querySelector(".door")
+
 init()
 console.log('ok')
 function init(){
+    // Création du personnage et placement du personnage
     perso = document.createElement("img")
-    let srcImgPerso = "sprites/perso0.png"
+    let srcImgPerso = "sprites/face_sprite0.png"
     perso.setAttribute('src', srcImgPerso)
     perso.setAttribute('id', 'charac')
     persoX = 0
@@ -12,61 +14,69 @@ function init(){
     document.querySelector('body').appendChild(perso)
     placePerso()
 }
-
+// Déplacement du personnage
 window.addEventListener('keydown', (e) => {
     console.log(e.keyCode)
     switch (e.keyCode) {
+        // Vers le haut
         case 90 :
-            persoY -= 64
+            persoY -= 20
             console.log(persoY)
             if (persoY < 0 || persoY > window.innerHeight) {
-                persoY += 64
+                persoY += 20
             }
             else{
                 placePerso()
             }
-            srcImgPerso = "sprites/perso2.png"
+            srcImgPerso = "sprites/back_sprite" + n + ".png"
             break;
+            // À gauche
         case 68 :
-            persoX += 64
+            persoX += 20
             console.log(persoX)
             if (persoX < 0 || persoX > window.innerWidth-1) {
-                persoX -= 64
+                persoX -= 20
             }
             else{
                 placePerso()
             }
-            srcImgPerso = "sprites/perso3.png"
+            srcImgPerso = "sprites/right_sprite" + n + ".png"
             break;
+            // Vers le bas
         case 83 :
-            persoY += 64
+            persoY += 20
             console.log(persoY)
+            srcImgPerso = "sprites/face_sprite" + n + ".png"
             if (persoY < 0 || persoY > window.innerHeight) {
-                persoY -= 64
+                persoY -= 20
             }
             else{
                 placePerso()
             }
-            srcImgPerso = "sprites/perso0.png"
-            perso.setAttribute('src', srcImgPerso)
+
             break;
+            // À droite
         case 81 :
-            persoX -= 64
+            persoX -= 20
             console.log(persoX)
             if (persoX < 0 || persoX > window.innerWidth-1) {
-                persoX += 64
+                persoX += 20
             }
             else{
                 placePerso()
             }
-            srcImgPerso = "sprites/perso1.png"
+            srcImgPerso = "sprites/left_sprite" + n + ".png"
             break;
         default:
     }
+    n++
+    if (n == 4) {
+        n = 0
+    }
     perso.setAttribute('src', srcImgPerso)
-    if (persoX == 400 && persoY == 400) {
-        //setTimeout(enterHouse(), 5000)
+    if (persoX == 384 && persoY == 384) {
         door.setAttribute('src','sprites/opendoor.png')
+        enterHouse()
     }
 })
 function enterHouse(){
