6f757aed73fb81b0651f2d4fcff31bc40af27750
Constance Devanne <devconstance@gmail.com>
2018-04-10 11:19:56 +0200

    new

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,31 @@
 /*Choix du joueur*/
 let choice = document.querySelectorAll(".choice")
 let player = 0
+let avatar = new Image()
 
 for(let i = 0; i < choice.length; i++) {
   choice[i].addEventListener('click', function(){
     if (i == 0) {
       let ask = prompt("Entrez votre pseudo !")
+      avatar.src = "../images/"
       player = (`Bienvenue ${ask} !`)
     }
     else {
       ask = prompt("Entrez votre pseudo !")
+      avatar.src = "../images/"
       player = (`Bienvenue ${ask} !`)
     }
     document.querySelector("#player").innerHTML = player
   })
 }
+
+
 /*explications et choix sur du monde*/
 let world = document.querySelectorAll(".items")
-let isActive = 0
+let mapExplain = document.querySelector(".mapExplain")
 
 for(let i = 0; i < world.length; i++) {
   world[i].addEventListener( 'click', function() {
-    if(isActive == 0) {
-        world[i].style.border=("2px solid red");
-        world[i].style.borderRadius = ("50%");
-        isActive=1;
-    } else{
-        world[i].style.border = ("none");
-        isActive = 0;
-    }
+    mapExplain.classList.toggle('current');
   })
 }
