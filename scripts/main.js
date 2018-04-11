916df36ac2818dea15d60fb721f9b8bc51bd1bd5
Constance Devanne <devconstance@gmail.com>
2018-04-11 23:55:57 +0200

    Choix perso

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,8 @@
 //Choix du joueur
 let choice = document.querySelectorAll(".choice")
 let pseudo = document.querySelector('#pseudo')
-let player = 0
+let player, ask, image
 let avatar = [[new Image(), new Image(), new Image(), new Image()],[new Image(), new Image(), new Image(), new Image()]]
-let ask = 0
-let image = 0
 let avatarChoice = 0
 
 for(let i = 0; i < choice.length; i++) {
@@ -12,15 +10,14 @@ for(let i = 0; i < choice.length; i++) {
     if (i == 0) {
       choice[0].style.border = ("2px solid #1C2164")
       choice[1].style.border = ("0px")
+      //Rentrer son pseudo
       pseudo.addEventListener('keypress', (e) => {
-        switch (e.keyCode) {
-          case 13:
+        if(e.keyCode == 13) {
             ask = document.querySelector('#pseudo').value
-            console.log("zizi")
             avatar[0].src = ["../sprites/perso0.png","../sprites/perso1.png","../sprites/perso2.png","../sprites/perso3.png"]
             player = (`Bienvenue ${ask} !`)
             image = 0
-            avatarChoice ++
+            avatarChoice += 1
             document.querySelector("#player").innerHTML = player
         }
       }
@@ -29,14 +26,14 @@ for(let i = 0; i < choice.length; i++) {
     else {
       choice[1].style.border = ("2px solid #1C2164")
       choice[0].style.border = ("0px")
+      //Rentrer son pseudo
       pseudo.addEventListener('keypress', (e) => {
-        switch (e.keyCode) {
-          case 13:
+        if(e.keyCode == 13) {
             ask = document.querySelector('#pseudo').value
             avatar[1].src = "../images/"
             player = (`Bienvenue ${ask} !`)
             image = 1
-            avatarChoice ++
+            avatarChoice += 1
             document.querySelector("#player").innerHTML = player
         }
       }
@@ -56,9 +53,8 @@ let mapExplain = document.querySelector(".mapExplain")
 
 for(let i = 0; i < world.length; i++) {
   world[i].addEventListener( 'click', function() {
-    if(avatarChoice == 1) {
+    if(avatarChoice > 0) {
       mapExplain.classList.toggle('current');
-      console.log("zizi")
 
     }
     else {
